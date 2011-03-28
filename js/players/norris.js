edcb1bf382b7a4222ea74f663413d45ee73f14a3
Anthony Deaver <lordbishop+github@gmail.com>
2011-03-28 12:02:04 -0400

    Initial Import'

---
+++ js/players/norris.js
@@ -0,0 +1,10 @@
+//Chuck Norris
+
+function init() {
+    var strength = 7;
+    var dexterity = 9;
+    var intutition = 8;
+    var constitution = 7;
+    var wisdom = 9;
+    var charisma = 5;
+}
